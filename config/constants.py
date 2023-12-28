c9b281e9409e37a7c3a50e675ecd3f868cfca770
Marius <marius.arlauskas@surfsharkteam.com>
2023-12-28 09:33:35 +0200

    remove unnecessary boilerplate

--- config/constants.py
+++ config/constants.py
@@ -1,7 +1,1 @@
-GOOGLE_CREDENTIALS = 'GOOGLE_APPLICATION_CREDENTIALS'
-SCOPES = [
-    'https://www.googleapis.com/auth/bigquery',
-    'https://www.googleapis.com/auth/drive',
-]
-PROJECT_ID = 'surfshark-analytics'
 HUBBLE_CONSTANT = 42
